daa2b9be492c24e71838f177be45c48db7895b62
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2021-05-26 21:18:36 +0200

    improve memory usage for prediction

--- evaluation/batchBALD_tests.py
+++ evaluation/batchBALD_tests.py
@@ -39,9 +39,9 @@ class Model:
         self.loss = nn.NLLLoss()
         self.batch_size = batch_size
 
-    def fit(self, X, y, epochs=50):
-        X = torch.from_numpy(X).reshape(-1,1,28,28).float()
-        y = torch.from_numpy(y).long()
+    def fit(self, X, y, epochs=50, device=None):
+        X = torch.from_numpy(X).reshape(-1,1,28,28).float().to(device)
+        y = torch.from_numpy(y).long().to(device)
  
         for _ in range(epochs):
             for i in range(0, len(X), self.batch_size):
@@ -52,12 +52,26 @@ class Model:
                 loss.backward()
                 self.optimizer.step()
 
-    def predict(self, X):
-        pred = self.net(torch.from_numpy(X).reshape(-1,1,28,28).float())
-        return pred.argmax(dim=1).detach().numpy()
+    def predict(self, X, batch_size=500, device=None):
+        if len(X) <= batch_size:
+            return self.predict_proba_batch(X, device).argmax(axis=1)
+        
+        pred = self.predict_proba_batch(X[:batch_size], device).argmax(axis=1)
+        for i in range(batch_size, len(X), batch_size):
+            pred = np.concatenate((pred, self.predict_proba_batch(X[i:i+batch_size], device).argmax(axis=1)))
+        return pred
 
-    def predict_proba(self, X):
-        return self.net(torch.from_numpy(X).reshape(-1,1,28,28).float()).detach().numpy()
+    def predict_proba(self, X, batch_size=500, device=None):
+        if len(X) <= batch_size:
+            return np.exp(self.predict_proba_batch(X, device))
+
+        pred = self.predict_proba_batch(X[:batch_size], device)
+        for i in range(batch_size, len(X), batch_size):
+            pred = np.concatenate((pred, self.predict_proba_batch(X[i:i+batch_size], device)))
+        return np.exp(pred)
+
+    def predict_proba_batch(self, batch, device=None):
+        return self.net(torch.from_numpy(batch).reshape(-1,1,28,28).float().to(device)).detach().cpu().numpy()
 
 
 class query_strat:
@@ -68,15 +82,16 @@ class query_strat:
     def select(self, label_ind, unlab_ind, batch_size, model_copy, query_strategy):
         return query_strategy.select(label_ind, unlab_ind, model_copy, batch_size, self.num_samples, self.K)
 
-def test_MNIST(batchsize, path, saving_path, k):
+def test_MNIST(batchsize, path, saving_path, k, num_samples):
     """
     path: directory where X and y of the MNIST dataset are stored (as pickle files)
     """
     X = pickle.load(open(path + "/X.pkl", "rb"))
     y = pickle.load(open(path + "/y.pkl", "rb"))
 
-    model = Model(BayesianNet())
+    net = BayesianNet()
+    model = Model(net)
 
     runner = ExperimentRunner(X, y, saving_path)
     runner.run_one_strategy("QueryInstanceBatchBALD", 6, None, 250, batchsize, 1/7, 2/6000, model,
-                            "batchBALD_" + str(batchsize) + "batch", True, "batchBALD", query_strat(k,5000))
+                            "batchBALD_" + str(batchsize) + "batch", True, "batchBALD", query_strat(k,num_samples))
