443ca669a78f266d07bbbd34a54e577ef722214e
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2021-06-11 12:40:34 +0200

    add LAL_RL strategy

---
+++ alipy/query_strategy/LAL_RL/Agent.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+class Net(nn.Module):
+    def __init__(self, candidate_size, bias_initialization=None):
+        super().__init__()
+        self.fc1 = nn.Linear(candidate_size, 10)
+        self.fc2 = nn.Linear(13, 5)
+        self.fc3 = nn.Linear(5, 1)
+        
+        if bias_initialization is not None:
+            self.fc3.bias = torch.nn.Parameter(torch.tensor(bias_initialization, dtype=torch.float))
+        
+    def forward(self, t):
+        state = t[:,:-3]
+        action = t[:,-3:]
+        t = torch.sigmoid(self.fc1(state))
+        t = torch.cat((t,action), dim=1)
+        t = torch.sigmoid(self.fc2(t))
+        t = self.fc3(t)
+        return t
+
+
+class Agent:
+    def __init__(self, n_state_estimation=30, learning_rate=1e-3, batch_size=32, bias_average=0,
+               gamma=0.999, device=None):
+        self.net = Net(n_state_estimation, bias_average).to(device)
+        self.target_net = Net(n_state_estimation, bias_average).to(device)
+        self.target_net.eval()
+        self.device = device
+        
+        # copy weihts from training net to target net
+        self.target_net.load_state_dict(self.net.state_dict())
+        
+        # create loss function and optimizer
+        self.loss = nn.MSELoss()
+        self.optimizer = optim.Adam(self.net.parameters(), lr=learning_rate)
+        self.batch_size = batch_size
+        self.gamma = gamma
+        
+    def train(self, minibatch):
+        max_prediction_batch = []
+        
+        for i, next_classifier_state in enumerate(minibatch.next_classifier_state):
+            # Predict q-value function value for all available actions
+            n_next_actions = np.shape(minibatch.next_action_state[i])[1]
+            next_classifier_state = np.repeat([next_classifier_state], n_next_actions, axis=0)
+            next_classifier_state = np.concatenate((next_classifier_state, 
+                                                    minibatch.next_action_state[i].transpose()), axis=1)
+            input_tensor = torch.tensor(next_classifier_state, dtype=torch.float, device=self.device)
+            
+            # Use target_estimator
+            target_predictions = self.target_net(input_tensor)
+            
+            # Use estimator
+            predictions = self.net(input_tensor)
+            
+            target_predictions = np.ravel(target_predictions.detach().cpu().numpy())
+            predictions = np.ravel(predictions.detach().cpu().numpy())
+            
+            # Follow Double Q-learning idea of van Hasselt, Guez, and Silver 2016
+            # Select the best action according to predictions of estimator
+            best_action_by_estimator = np.random.choice(np.where(predictions == np.amax(predictions))[0])
+            # As the estimate of q-value of the best action, 
+            # take the prediction of target estimator for the selecting action
+            max_target_prediction_i = target_predictions[best_action_by_estimator]
+            max_prediction_batch.append(max_target_prediction_i)
+            
+        expected_state_action_values = minibatch.reward + self.gamma*np.array(max_prediction_batch)
+        expected_state_action_values = torch.tensor(expected_state_action_values, dtype=torch.float, device=self.device)
+        
+        input_tensor = np.concatenate((minibatch.classifier_state, minibatch.action_state), axis=1)
+        input_tensor = torch.from_numpy(input_tensor).to(self.device).float()
+        net_output = self.net(input_tensor)
+        net_output = net_output.flatten()
+        
+        td_errors = net_output - expected_state_action_values
+        
+        # actually train the network
+        loss = self.loss(net_output, expected_state_action_values)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        
+        return td_errors.detach().cpu().numpy()
+    
+    def get_action(self, classifier_state, action_state):
+        input_tensor = np.concatenate((np.repeat(classifier_state[None,:], action_state.shape[1], axis=0), 
+                                       action_state.transpose()), axis=1)
+        input_tensor = torch.tensor(input_tensor, dtype=torch.float, device=self.device)
+        predictions = self.net(input_tensor)
+        predictions = predictions.flatten()
+        
+        predictions = predictions.detach().cpu().numpy()
+        max_action = np.random.choice(np.where(predictions == predictions.max())[0])
+        
+        return max_action
+        
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.net.state_dict())
+    
+    def save_net(self, path):
+        torch.save(self.net.state_dict(), path)
---
+++ alipy/query_strategy/LAL_RL/learn_strategy.py
@@ -0,0 +1,212 @@
+from .envs import LalEnvTargetAccuracy
+from .datasets import DatasetUCI
+from .helpers import ReplayBuffer
+from .Agent import Agent, Net
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import scipy
+import collections
+from ..base import BaseIndexQuery
+
+from tqdm import tqdm
+
+import torch
+
+
+class StrategyLearner:
+    def __init__(self, path, possible_dataset_names, n_state_estimation=30, size=-1, subset=-1,
+                 quality_method=metrics.accuracy_score, tolerance_level=0.98, model=None,
+                 replay_buffer_size = 1e4, prioritized_replay_exponent = 3) :
+        dataset = DatasetUCI(possible_dataset_names, n_state_estimation=n_state_estimation, subset=subset,
+                             size=size,path=path)
+        if model == None:
+            model = LogisticRegression()
+        self.env = LalEnvTargetAccuracy(dataset, model, quality_method=quality_method,
+                                        tolerance_level=tolerance_level)
+        self.n_state_estimation = n_state_estimation
+        self.replay_buffer = ReplayBuffer(buffer_size=replay_buffer_size, prior_exp=prioritized_replay_exponent)
+
+
+    def train_query_strategy(self, saving_path, file_name, warm_start_episodes=128, nn_updates_per_warm_start=100,
+                n_state_estimation=30, learning_rate=1e-3, batch_size=32, gamma=0.999, update_rate=100,
+                training_iterations=1000, episodes_per_iteration=10, updates_per_iteration=60,
+                epsilon_start=1, epsilon_end=0.1, epsilon_step=1000, device=None):
+        self.saving_path = saving_path + "/" + file_name
+        self.batch_size = batch_size
+        self.udpate_rate = update_rate
+        self.training_iterations = training_iterations
+        self.episodes_per_iteration = episodes_per_iteration
+        self.updates_per_iteration = updates_per_iteration
+        self.epsilon_start = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_step = epsilon_step
+
+        bias_average = self.run_warm_start_episodes(warm_start_episodes)
+        self.agent = Agent(n_state_estimation, learning_rate, batch_size, bias_average,
+               gamma, device)
+        self.train_agent(nn_updates_per_warm_start)
+        self.run_training_iterations()
+
+
+    def run_warm_start_episodes(self, n_episodes):
+        # Keep track of episode duration to compute average
+        episode_durations = []
+        for _ in tqdm(range(n_episodes), desc="Warmstart episodes"):
+            # Reset the environment to start a new episode
+            # classifier_state contains vector representation of state of the environment (depends on classifier)
+            # next_action_state contains vector representations of all actions available to be taken at the next step
+            classifier_state, next_action_state = self.env.reset()
+            terminal = False
+            episode_duration = 0
+            # before we reach a terminal state, make steps
+            while not terminal:
+                # Choose a random action
+                action = np.random.randint(0, self.env.n_actions)
+                # taken_action_state is a vector corresponding to a taken action
+                taken_action_state = next_action_state[:,action]
+                next_classifier_state, next_action_state, reward, terminal = self.env.step(action)
+                # Store the transition in the replay buffer
+                self.replay_buffer.store_transition(classifier_state, 
+                                            taken_action_state, 
+                                            reward, next_classifier_state, 
+                                            next_action_state, terminal)
+                # Get ready for next step
+                classifier_state = next_classifier_state
+                episode_duration += 1 
+            episode_durations.append(episode_duration)
+        # compute the average episode duration of episodes generated during the warm start procedure
+        av_episode_duration = np.mean(episode_durations)
+        print('\nAverage episode duration = ', av_episode_duration)
+
+        return -av_episode_duration/2
+
+
+    def train_agent(self, n_of_updates):
+        for _ in tqdm(range(n_of_updates), desc="Train q-net", leave=False):
+            # Sample a batch from the replay buffer proportionally to the probability of sampling.
+            minibatch = self.replay_buffer.sample_minibatch(self.batch_size)
+            # Use batch to train an agent. Keep track of temporal difference errors during training.
+            td_error = self.agent.train(minibatch)
+            # Update probabilities of sampling each datapoint proportionally to the error.
+            self.replay_buffer.update_td_errors(td_error, minibatch.indeces)
+
+
+    def run_training_iterations(self):
+        i_episode = 0
+
+        for iteration in tqdm(range(self.training_iterations), desc="Train iterations"):
+            # GENERATE NEW EPISODES
+            # Compute epsilon value according to the schedule.
+            epsilon = max(self.epsilon_end, self.epsilon_start-iteration*(self.epsilon_start-self.epsilon_end)/self.epsilon_step)
+            # Simulate training episodes.
+            for _ in tqdm(range(self.episodes_per_iteration), desc="Episodes", leave=False):
+                # Reset the environment to start a new episode.
+                classifier_state, next_action_state = self.env.reset()
+                terminal = False
+                # Keep track of stats of episode to analyse it in tensorboard.
+                episode_reward = 0
+                episode_duration = 0
+                # Run an episode.
+                while not terminal:
+                    # Let an agent choose an action or with epsilon probability, take a random action.
+                    if np.random.ranf() < epsilon: 
+                        action = np.random.randint(0, self.env.n_actions)
+                    else:
+                        action = self.agent.get_action(classifier_state, next_action_state)
+                    
+                    # taken_action_state is a vector that corresponds to a taken action
+                    taken_action_state = next_action_state[:,action]
+                    # Make another step.
+                    next_classifier_state, next_action_state, reward, terminal = self.env.step(action)
+                    # Store a step in replay buffer
+                    self.replay_buffer.store_transition(classifier_state, 
+                                                taken_action_state, 
+                                                reward, 
+                                                next_classifier_state, 
+                                                next_action_state, 
+                                                terminal)
+                    # Change a state of environment.
+                    classifier_state = next_classifier_state
+                    # Keep track of stats and add summaries to tensorboard.
+                    episode_reward += reward
+                    episode_duration += 1
+                i_episode += 1
+                    
+            # NEURAL NETWORK UPDATES
+            self.train_agent(self.updates_per_iteration)
+
+        self.agent.save_net(self.saving_path)
+
+
+
+class QueryInstanceLAL_RL(BaseIndexQuery):
+    def __init__(self, X, y, model_path, n_state_estimation=None, pred_batch=128, device=None):
+        super(QueryInstanceLAL_RL, self).__init__(X, y)
+        state_dict = torch.load(model_path, map_location=device)
+        self.pred_batch = pred_batch
+        self.device = device
+        if n_state_estimation == None:
+            self.n_state_estimation = state_dict[list(state_dict.keys())[0]].size(1)
+        else:
+            self.n_state_estimation = n_state_estimation
+        self.net = Net(self.n_state_estimation,0)
+        self.net.load_state_dict(state_dict)
+        self.net.to(device)
+        self.net.eval()
+
+    def select(self, label_index, unlabel_index, model=None, batch_size=1):
+        assert (batch_size > 0)
+        assert (isinstance(unlabel_index, collections.Iterable))
+        if len(unlabel_index) <= batch_size:
+            return unlabel_index
+        assert len(unlabel_index) > self.n_state_estimation
+        unlabel_index = np.asarray(unlabel_index)
+
+        # initialize the model and train it if necessary
+        if model == None:
+            model = LogisticRegression()
+            model.fit(self.X[label_index], self.y[label_index])
+        
+        # set aside some unlabeled data for the state representation
+        state_indices = np.random.choice(len(unlabel_index), size=self.n_state_estimation, replace=False)
+        unlabel_index = unlabel_index[np.array([x for x in range(len(unlabel_index)) if x not in state_indices])]
+
+        # create the state
+        predictions = model.predict_proba(self.X[state_indices])[:,0]
+        predictions = np.array(predictions)
+        idx = np.argsort(predictions)
+        state = predictions[idx]
+
+        #create the actions
+        a1 = model.predict_proba(self.X[unlabel_index])[:,0]
+
+        # calculate distances
+        data = self.X[np.concatenate((label_index,unlabel_index),axis=0)]
+        distances = scipy.spatial.distance.pdist(data, metric='cosine')
+        distances = scipy.spatial.distance.squareform(distances)
+        indeces_known = np.arange(len(label_index))
+        indeces_unknown = np.arange(len(label_index), len(label_index)+len(unlabel_index))
+        a2 = np.mean(distances[indeces_unknown,:][:,indeces_unknown],axis=0)
+        a3 = np.mean(distances[indeces_known,:][:,indeces_unknown],axis=0)
+
+        actions = np.concatenate(([a1], [a2], [a3]), axis=0).transpose()
+
+        # calculate the q-values according to the q-network
+        # first transform the state and actions for the network
+        state = np.repeat([state], actions.shape[0], axis=0)
+        state_actions = np.concatenate((state,actions),axis=1)
+        input_tensor = torch.tensor(state_actions, dtype=torch.float, device=self.device)
+
+        # get the prediction from the network
+        pred = self.net(input_tensor[:self.pred_batch])
+        for i in range(self.pred_batch, input_tensor.size(0), self.pred_batch):
+            pred = torch.cat((pred, self.net(input_tensor[i:i+self.pred_batch])))
+        pred = pred.flatten()
+
+        # sort the actions with respect to their q-value
+        idx = pred.argsort(descending=True)
+        idx = idx[:batch_size].detach().cpu().numpy()
+
+        # return the correspoding indeces from the unlabeld index
+        return unlabel_index[idx]
