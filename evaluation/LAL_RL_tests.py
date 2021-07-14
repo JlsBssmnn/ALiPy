d01d809611af582c6bc94969d9c560ef87d5ded5
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2021-07-14 16:31:49 +0200

    add function to search for hyper parameters for LAL_RL

--- evaluation/LAL_RL_tests.py
+++ evaluation/LAL_RL_tests.py
@@ -1,16 +1,20 @@
 from alipy import ToolBox
 import pickle
 from tqdm.auto import tqdm
-from alipy.query_strategy import QueryInstanceLAL_RL
+from alipy.query_strategy import QueryInstanceLAL_RL, LAL_RL_StrategyLearner
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 from sklearn.base import clone
+from scipy.stats import uniform
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import RandomizedSearchCV
 
 # with default parameters for LogisticRegression we likely get a ConvergenceWarning
 import warnings
 warnings.filterwarnings("ignore")
 
 import numpy as np
+import os
 
      
 def test_LAL_RL(save_path, save_name, path, strategy_name, rounds=10, test_ratio=0.2, init_lab=2, num_of_queries=100, model=None, **kwargs):
@@ -102,3 +106,104 @@ def test_LAL_RL(save_path, save_name, path, strategy_name, rounds=10, test_ratio
         
     # saving the results
     np.save(save_path + "/" + save_name + ".npy", quality_results)
+
+
+# finding the right hyper parameters for LAL_RL
+class Attr:
+    # class that stores fixed attributes
+    def __init__(self, dataset_path, possible_dataset_names, saving_path):
+        self.dataset_path = dataset_path
+        self.possible_dataset_names = possible_dataset_names
+        self.saving_path = saving_path
+        
+    def __str__(self):
+        s = ""
+        for key in self.__dict__:
+            s += key + "=" + str(getattr(self, key)) + ","
+        return s[:-1]
+
+
+class Model(BaseEstimator):
+    def __init__(self, attr, n_state_estimation, subset, tolerance_level, model, replay_buffer_size, 
+                 prioritized_replay_exponent, warm_start_episodes, nn_updates_per_warm_start, learning_rate, 
+                 batch_size, gamma, update_rate, training_iterations, episodes_per_iteration, updates_per_iteration, 
+                 epsilon_start, epsilon_end, epsilon_step):
+        self.attr = attr
+        self.n_state_estimation = n_state_estimation
+        self.subset = subset
+        self.tolerance_level = tolerance_level
+        self.model = model
+        self.replay_buffer_size = replay_buffer_size
+        self.prioritized_replay_exponent = prioritized_replay_exponent
+        self.warm_start_episodes = warm_start_episodes
+        self.nn_updates_per_warm_start = nn_updates_per_warm_start
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.update_rate = update_rate
+        self.training_iterations = training_iterations
+        self.episodes_per_iteration = episodes_per_iteration
+        self.updates_per_iteration = updates_per_iteration
+        self.epsilon_start = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_step = epsilon_step
+        
+        self.LAL_RL = LAL_RL_StrategyLearner(attr.dataset_path, attr.possible_dataset_names, n_state_estimation,
+                                             subset=subset, tolerance_level=tolerance_level,
+                                             model = None if model==None else svm.SVC(),
+                                             replay_buffer_size=replay_buffer_size,
+                                             prioritized_replay_exponent=prioritized_replay_exponent)
+    
+    def fit(self, X, y):
+        file_name = "n=" + str(self.n_state_estimation) + ",r=" + str(self.replay_buffer_size) + ",g=" + str(self.gamma) + ".txt"
+        if os.path.exists(os.path.join(self.attr.saving_path, file_name)):
+            return
+        
+        f = open(os.path.join(self.attr.saving_path, file_name), "w")
+        for key in self.__dict__:
+            f.write(key + "=" + str(getattr(self, key)) + "\n")
+        f.close()
+        
+        self.LAL_RL.train_query_strategy(self.attr.saving_path, file_name[:-4], self.warm_start_episodes, 
+                self.nn_updates_per_warm_start, self.learning_rate, self.batch_size, self.gamma, self.update_rate,
+                self.training_iterations, self.episodes_per_iteration, self.updates_per_iteration,
+                self.epsilon_start, self.epsilon_end, self.epsilon_step)
+        
+        test_LAL_RL(self.attr.saving_path, file_name[:-4], os.path.join(self.attr.dataset_path, "adult.p"), 
+                    "QueryInstanceLAL_RL", 5, 0.3, 4, model_path=os.path.join(self.attr.saving_path, file_name[:-4]+".pt"))
+    
+    def score(self, X, y):
+        # we don't use the score function, this is just arbitrary output
+        return self.n_state_estimation
+
+
+def search_hyper_parameters(dataset_path, possible_dataset_names, saving_path, iterations):
+    param = dict(
+        n_state_estimation = range(25,50),
+        subset = [-1,0,1],
+        tolerance_level = uniform(loc=0.94, scale=0.05),
+        model = [None, "svm"],
+        replay_buffer_size = range(5000, 10000),
+        prioritized_replay_exponent = [1,2,3],
+        warm_start_episodes = range(32,300),
+        nn_updates_per_warm_start = range(0,200),
+        learning_rate = [1e-3, 1e-4],
+        batch_size = range(8,129),
+        gamma = uniform(loc=0.8, scale=0.199),
+        update_rate = range(1,6),
+        training_iterations = range(750,1001),
+        episodes_per_iteration = range(5,20),
+        updates_per_iteration = range(30,200),
+        epsilon_start = uniform(loc=0.8, scale=0.2),
+        epsilon_end = uniform(loc=0, scale=0.2),
+        epsilon_step = range(750,1001)
+    )
+
+    attr = Attr(dataset_path, possible_dataset_names, saving_path)
+    est = Model(attr, n_state_estimation=1, subset=-1, tolerance_level=0.98, model=None, replay_buffer_size=1e4, 
+                    prioritized_replay_exponent=3, warm_start_episodes=128, nn_updates_per_warm_start=100, learning_rate=1e-3, 
+                    batch_size=32, gamma=0.999, update_rate=100, training_iterations=1000, episodes_per_iteration=10,
+                    updates_per_iteration=60, epsilon_start=1, epsilon_end=0.1, epsilon_step=1000)
+    clf = RandomizedSearchCV(est, param, n_iter=iterations, refit=False, cv=2)
+
+    clf.fit([[1,2], [3,4]], [1,0])
